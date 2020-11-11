4eda8ba75fe0ddea5de75c88f7252c8e8b8b48fe
mgnsm <mgns_m@yahoo.com>
2020-11-11 22:15:18 +0100

    Implemented IFormattable.

--- Tests/Millistream.Streaming.DataTypes.UnitTests/NumberTests.cs
+++ Tests/Millistream.Streaming.DataTypes.UnitTests/NumberTests.cs
@@ -52,13 +52,13 @@ namespace Millistream.Streaming.DataTypes.UnitTests
             Assert.IsFalse(number4.IsNegative);
             Assert.AreEqual(4, number4.GetPrecision());
             Assert.AreEqual(2, number4.Scale);
-            Assert.AreEqual("40.05", number4.ToString());
+            Assert.AreEqual("40.05", number4.ToString(CultureInfo.InvariantCulture));
 
             Number number5 = new Number(new BigInteger(1010000007), 7);
             Assert.IsFalse(number5.IsNegative);
             Assert.AreEqual(10, number5.GetPrecision());
             Assert.AreEqual(7, number5.Scale);
-            Assert.AreEqual("101.0000007", number5.ToString());
+            Assert.AreEqual("101.0000007", number5.ToString(CultureInfo.InvariantCulture));
 
             Number number6 = new Number(new BigInteger(0), 0);
             Assert.IsFalse(number6.IsNegative);
@@ -210,12 +210,164 @@ namespace Millistream.Streaming.DataTypes.UnitTests
             Assert.AreEqual(Number.Abs(Number.Null), Number.Null);
         }
 
+        [TestMethod]
+        public void FormatNumbersTest()
+        {
+            string decimalSeparator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+
+            Assert.AreEqual($"123{decimalSeparator}45", new Number(12345, 2).ToString());
+            Assert.AreEqual($"123{decimalSeparator}45", new Number(12345, 2).ToString());
+            Assert.AreEqual($"123{decimalSeparator}4500", new Number(1234500, 4).ToString());
+            Assert.AreEqual($"0123{decimalSeparator}4500", CreateNumber("0123.4500").ToString());
+            Assert.AreEqual($"00123{decimalSeparator}45", CreateNumber("00123.45").ToString());
+            Assert.AreEqual($"9999999999999999999999999999999999999999{decimalSeparator}9999999999", CreateNumber("9999999999999999999999999999999999999999.9999999999").ToString());
+            Assert.AreEqual($"-9999999999999999999999999999999999999999{decimalSeparator}9999999999", CreateNumber("-9999999999999999999999999999999999999999.9999999999").ToString());
+            Assert.AreEqual("010101", CreateNumber("010101").ToString());
+            Assert.AreEqual($"-10000{decimalSeparator}4", new Number(-100004, 1).ToString());
+            Assert.AreEqual("NULL", Number.Null.ToString());
+            Assert.AreEqual($"153{decimalSeparator}10000", new Number(15310000, 5).ToString());
+            Assert.AreEqual($"-0153{decimalSeparator}10000", CreateNumber("-0153.10000").ToString());
+            Assert.AreEqual($"-0153{decimalSeparator}10001", CreateNumber("-0153.10001").ToString());
+
+            Assert.AreEqual("123,45", new Number(12345, 2).ToString(null, new CultureInfo("sv")));
+            Assert.AreEqual("00123,45", CreateNumber("00123.45").ToString(null, new CultureInfo("da")));
+            Assert.AreEqual("-0153,10001", CreateNumber("-0153.10001").ToString(null, new CultureInfo("fr")));
+            Assert.AreEqual("-978.453", new Number(-978453, 3).ToString(null, new CultureInfo("en-US")));
+
+            //"C" or "c" - Currency
+            Assert.AreEqual("$123.46", new Number(123456, 3).ToString("C", new CultureInfo("en-US")));
+            Assert.AreEqual("123,46 €", new Number(123456, 3).ToString("C", new CultureInfo("fr-FR")));
+            Assert.AreEqual("¥123", new Number(123456, 3).ToString("C", new CultureInfo("ja-JP")));
+            Assert.AreEqual("($123.456)", new Number(-123456, 3).ToString("C3", new CultureInfo("en-US")));
+            Assert.AreEqual("-123,456 €", new Number(-123456, 3).ToString("C3", new CultureInfo("fr-FR")));
+            Assert.AreEqual("-¥123.456", new Number(-123456, 3).ToString("C3", new CultureInfo("ja-JP")));
+            Assert.AreEqual(ulong.MaxValue.ToString("C2", new CultureInfo("sv-SE")), new Number(ulong.MaxValue, 0).ToString("C2", new CultureInfo("sv-SE")));
+            Assert.AreEqual(BigInteger.Parse("9999999999999999999999999999999999999999").ToString("C", new CultureInfo("en-US")),
+                CreateNumber("9999999999999999999999999999999999999999").ToString("C", new CultureInfo("en-US")));
+
+            //"D" or "d" - Decimal
+            Assert.AreEqual("0012.345", new Number(12345, 3).ToString("D7", CultureInfo.InvariantCulture));
+            Assert.AreEqual("1234567.89", new Number(123456789, 2).ToString("D0", CultureInfo.InvariantCulture));
+            Assert.AreEqual("00123", new Number(123, 0).ToString("D5", CultureInfo.InvariantCulture));
+            Assert.AreEqual("0.001", new Number(1, 3).ToString("D4", CultureInfo.InvariantCulture));
+            Assert.AreEqual("000.001", new Number(1, 3).ToString("D6", CultureInfo.InvariantCulture));
+
+            //"E" or "e" - Exponential (scientific)
+            Assert.AreEqual("1.052033E+003", new Number(10520329112756, 10).ToString("E", new CultureInfo("en-US")));
+            Assert.AreEqual("1,052033e+003", new Number(10520329112756, 10).ToString("e", new CultureInfo("fr-FR")));
+            Assert.AreEqual("-1.05e+003", new Number(-10520329112756, 10).ToString("e2", new CultureInfo("en-US")));
+            Assert.AreEqual("-1,05E+003", new Number(-10520329112756, 10).ToString("E2", new CultureInfo("fr-FR")));
+            Assert.AreEqual(0.123456789.ToString("G"), new Number(123456789, 9).ToString("G"));
+
+            //"F" or "f" - Fixed-point
+            Assert.AreEqual("1234.57", new Number(1234567, 3).ToString("F", new CultureInfo("en-US")));
+            Assert.AreEqual("1234,57", new Number(1234567, 3).ToString("F", new CultureInfo("de-DE")));
+            Assert.AreEqual("1234.0", new Number(1234, 0).ToString("F1", new CultureInfo("en-US")));
+            Assert.AreEqual("1234,0", new Number(1234, 0).ToString("F1", new CultureInfo("de-DE")));
+            Assert.AreEqual("-1234.5600", new Number(-123456, 2).ToString("F4", new CultureInfo("en-US")));
+            Assert.AreEqual("-1234,5600", new Number(-123456, 2).ToString("F4", new CultureInfo("de-DE")));
+            Assert.AreEqual("999999999999999.99999999999999900000", CreateNumber("999999999999999.999999999999999").ToString("F20", new CultureInfo("en-US")));
+
+            //"G" or "g" - General
+            Assert.AreEqual("-123.456", new Number(-123456, 3).ToString("G", new CultureInfo("en-US")));
+            Assert.AreEqual("-123,456", new Number(-123456, 3).ToString("G", new CultureInfo("sv-SE")));
+            Assert.AreEqual("123.5", new Number(1234546, 4).ToString("G4", new CultureInfo("en-US")));
+            Assert.AreEqual("123,5", new Number(1234546, 4).ToString("G4", new CultureInfo("sv-SE")));
+            Assert.AreEqual("-0,000000000000000000000000123456789", CreateNumber("-0.000000000000000000000000123456789").ToString("G", new CultureInfo("sv-SE")));
+
+            //"N" or "n" - Number
+            Assert.AreEqual("1,234.57", new Number(1234567, 3).ToString("N", new CultureInfo("en-US")));
+            Assert.AreEqual("1 234,57", new Number(1234567, 3).ToString("N", new CultureInfo("sv-SE")));
+            Assert.AreEqual("1,234.0", new Number(1234, 0).ToString("N1", new CultureInfo("en-US")));
+            Assert.AreEqual("1 234,0", new Number(1234, 0).ToString("N1", new CultureInfo("sv-SE")));
+            Assert.AreEqual("-1,234.560", new Number(-123456, 2).ToString("N3", new CultureInfo("en-US")));
+            Assert.AreEqual("-1 234,560", new Number(-123456, 2).ToString("N3", new CultureInfo("sv-SE")));
+
+            //"P" or "p" - Percent
+            Assert.AreEqual("100.00%", new Number(1, 0).ToString("P", new CultureInfo("en-US")));
+            Assert.AreEqual("100,00 %", new Number(1, 0).ToString("P", new CultureInfo("fr-FR")));
+            Assert.AreEqual("-39.7%", new Number(-39678, 5).ToString("P1", new CultureInfo("en-US")));
+            Assert.AreEqual("-39,7 %", new Number(-39678, 5).ToString("P1", new CultureInfo("fr-FR")));
+            Assert.AreEqual("-39,68 %", new Number(-39678, 5).ToString("P2", new CultureInfo("sv-SE")));
+            Assert.AreEqual("3.97%", new Number(39678, 6).ToString("P2", new CultureInfo("en-US")));
+            Assert.AreEqual("0.40%", new Number(39678, 7).ToString("P2", new CultureInfo("en-US")));
+            Assert.AreEqual(10.ToString("p"), new Number(10, 0).ToString("p"));
+            Assert.AreEqual(0.02M.ToString("p"), new Number(2, 2).ToString("p"));
+
+            //"0" - Zero placeholder
+            Assert.AreEqual("01235", new Number(12345678, 4).ToString("00000"));
+            Assert.AreEqual("0.46", new Number(45678, 5).ToString("0.00", new CultureInfo("en-US")));
+            Assert.AreEqual("0,46", new Number(45678, 5).ToString("0.00", new CultureInfo("fr-FR")));
+
+            //"#" - Digit placeholder
+            Assert.AreEqual("1235", new Number(12345678, 4).ToString("#####"));
+            Assert.AreEqual(".46", new Number(45678, 5).ToString("#.##", new CultureInfo("en-US")));
+            Assert.AreEqual(",46", new Number(45678, 5).ToString("#.##", new CultureInfo("fr-FR")));
+
+            //"." - Decimal point
+            Assert.AreEqual("0.46", new Number(45678, 5).ToString("0.00", new CultureInfo("en-US")));
+            Assert.AreEqual("0,46", new Number(45678, 5).ToString("0.00", new CultureInfo("fr-FR")));
+
+            //"," - Group separator and number scaling
+            Assert.AreEqual("2,147,483,647", new Number(2147483647, 0).ToString("##,#", new CultureInfo("en-US")));
+            Assert.AreEqual("2.147.483.647", new Number(2147483647, 0).ToString("##,#", new CultureInfo("es-ES")));
+            Assert.AreEqual("2,147", new Number(2147483647, 0).ToString("#,#,,", new CultureInfo("en-US")));
+            Assert.AreEqual("2.147", new Number(2147483647, 0).ToString("#,#,,", new CultureInfo("es-ES")));
+
+            //"%" - Percentage placeholder
+            Assert.AreEqual("%36.97", new Number(3697, 4).ToString("%#0.00", new CultureInfo("en-US")));
+            Assert.AreEqual("%36,97", new Number(3697, 4).ToString("%#0.00", new CultureInfo("el-GR")));
+            Assert.AreEqual("37.0 %", new Number(3697, 4).ToString("##.0 %", new CultureInfo("en-US")));
+            Assert.AreEqual("37,0 %", new Number(3697, 4).ToString("##.0 %", new CultureInfo("el-GR")));
+            Assert.AreEqual("3,7 %", new Number(3697, 5).ToString("##.0 %", new CultureInfo("el-GR")));
+
+            //"‰" - Per mille placeholder
+            Assert.AreEqual("36.97‰", new Number(3697, 5).ToString("#0.00‰", new CultureInfo("en-US")));
+            Assert.AreEqual("36,97‰", new Number(3697, 5).ToString("#0.00‰", new CultureInfo("ru-RU")));
+            Assert.AreEqual("3,6970‰", new Number(3697, 6).ToString("#0.0000‰", new CultureInfo("sv-SE")));
+            Assert.AreEqual("369,70‰", new Number(3697, 4).ToString("#0.00‰", new CultureInfo("sv-SE")));
+
+            //Exponential notation
+            Assert.AreEqual("98.8e4", new Number(987654, 0).ToString("#0.0e0", new CultureInfo("en-US")));
+            Assert.AreEqual("1.504e+03", new Number(150392311, 5).ToString("0.0##e+00", new CultureInfo("en-US")));
+
+            //"\" - Escape character
+            Assert.AreEqual("#987654#", new Number(987654, 0).ToString(@"\###00\#"));
+
+            //'string' - Literal string/delimiter
+            Assert.AreEqual("68 degrees", new Number(68, 0).ToString("# 'degrees'"));
+            Assert.AreEqual("68 degrees", new Number(68, 0).ToString("#' degrees'"));
+
+            //;	Section separator
+            Assert.AreEqual("12.35", new Number(12345, 3).ToString("#0.0#;(#0.0#);-\0-", new CultureInfo("en-US")));
+            Assert.AreEqual(0.ToString("#0.0#;(#0.0#);-\0-"), new Number(0, 0).ToString("#0.0#;(#0.0#);-\0-"));
+            Assert.AreEqual("(12.35)", new Number(-12345, 3).ToString("#0.0#;(#0.0#);-\0-", new CultureInfo("en-US")));
+            Assert.AreEqual("12.35", new Number(12345, 3).ToString("#0.0#;(#0.0#)", new CultureInfo("en-US")));
+            Assert.AreEqual("0.0", new Number(0, 0).ToString("#0.0#;(#0.0#)", new CultureInfo("en-US")));
+            Assert.AreEqual("(12.35)", new Number(-12345, 3).ToString("#0.0#;(#0.0#)", new CultureInfo("en-US")));
+
+            //Other character literals
+            Assert.AreEqual("68 °", new Number(68, 0).ToString("# °"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void RoundTripFormatting() => new Number(1, 0).ToString("R");
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void HexadecimalFormatting() => new Number(1, 0).ToString("X");
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void FormattingUsingAnUnknownFormatter() => new Number(1, 0).ToString("M");
+
         private static void ParseNumberTest(string s)
         {
             Assert.IsTrue(Number.TryParse(s, out Number number));
-            Assert.AreEqual(s, number.ToString());
+            Assert.AreEqual(s, number.ToString(CultureInfo.InvariantCulture));
             Assert.IsTrue(Number.TryParse(s.GetBytes(), out number));
-            Assert.AreEqual(s, number.ToString());
+            Assert.AreEqual(s, number.ToString(CultureInfo.InvariantCulture));
         }
 
         private static Number CreateNumber(string s) => Number.Parse(s.GetBytes());
