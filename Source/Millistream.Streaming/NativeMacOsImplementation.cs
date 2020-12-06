2b84b0be1381cc38955a716759c70b9a3c473007
mgnsm <mgns_m@yahoo.com>
2020-12-06 21:46:30 +0100

    Invokation of the native mdf_message_add_date2 and mdf_message_add_time3 methods.

--- Source/Millistream.Streaming/NativeMacOsImplementation.cs
+++ Source/Millistream.Streaming/NativeMacOsImplementation.cs
@@ -70,10 +70,16 @@ namespace Millistream.Streaming
             public static extern int mdf_message_add_date(IntPtr message, uint tag, string value);
 
             [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+            public static extern int mdf_message_add_date2(IntPtr message, uint tag, int year, int mon, int day);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_add_time(IntPtr message, uint tag, string value);
 
             [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_add_time2(IntPtr message, uint tag, int hour, int min, int sec, int msec);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+            public static extern int mdf_message_add_time3(IntPtr message, uint tag, int hour, int min, int sec, int nsec);
 
             [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_get_num(IntPtr message);
@@ -95,12 +101,14 @@ namespace Millistream.Streaming
         public int mdf_get_property(IntPtr handle, MDF_OPTION option, ref IntPtr value) => NativeUnixMethods.mdf_get_property(handle, option, ref value);
         public int mdf_message_add(IntPtr message, ulong instrument_reference, int message_reference) => NativeUnixMethods.mdf_message_add(message, instrument_reference, message_reference);
         public int mdf_message_add_date(IntPtr message, uint tag, string value) => NativeUnixMethods.mdf_message_add_date(message, tag, value);
+        public int mdf_message_add_date2(IntPtr message, uint tag, int year, int mon, int day) => NativeUnixMethods.mdf_message_add_date2(message, tag, year, mon, day);
         public int mdf_message_add_int(IntPtr message, uint tag, long value, int decimals) => NativeUnixMethods.mdf_message_add_int(message, tag, value, decimals);
         public int mdf_message_add_list(IntPtr message, uint tag, string value) => NativeUnixMethods.mdf_message_add_list(message, tag, value);
         public int mdf_message_add_numeric(IntPtr message, uint tag, string value) => NativeUnixMethods.mdf_message_add_numeric(message, tag, value);
         public int mdf_message_add_string(IntPtr message, uint tag, string value) => NativeUnixMethods.mdf_message_add_string(message, tag, value);
         public int mdf_message_add_time(IntPtr message, uint tag, string value) => NativeUnixMethods.mdf_message_add_time(message, tag, value);
         public int mdf_message_add_time2(IntPtr message, uint tag, int hour, int min, int sec, int msec) => NativeUnixMethods.mdf_message_add_time2(message, tag, hour, min, sec, msec);
+        public int mdf_message_add_time3(IntPtr message, uint tag, int hour, int min, int sec, int nsec) => NativeUnixMethods.mdf_message_add_time3(message, tag, hour, min, sec, nsec);
         public int mdf_message_add_uint(IntPtr message, uint tag, ulong value, int decimals) => NativeUnixMethods.mdf_message_add_uint(message, tag, value, decimals);
         public IntPtr mdf_message_create() => NativeUnixMethods.mdf_message_create();
         public int mdf_message_del(IntPtr message) => NativeUnixMethods.mdf_message_del(message);
