e6eb20f7c49abc2a2b0314f4d3bd1c3bb205f4dd
mgnsm <mgns_m@yahoo.com>
2021-01-05 14:17:54 +0100

    Implemented Move.

--- Source/Millistream.Streaming/INativeImplementation.cs
+++ Source/Millistream.Streaming/INativeImplementation.cs
@@ -35,6 +35,7 @@ namespace Millistream.Streaming
         int mdf_message_get_num_active(IntPtr message);
         int mdf_message_send(IntPtr handle, IntPtr message);
         int mdf_message_set_compression_level(IntPtr message, int level);
+        int mdf_message_move(IntPtr src, IntPtr dst, ulong insref_src, ulong insref_dst);
         int mdf_message_serialize(IntPtr message, ref IntPtr result);
         int mdf_message_deserialize(IntPtr message, string data);
     }
