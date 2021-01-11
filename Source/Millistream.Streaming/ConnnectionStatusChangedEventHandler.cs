e5933c36fdaaf3f3c2bfd25267cf82319d51a87f
mgnsm <mgns_m@yahoo.com>
2021-01-11 10:38:37 +0100

    Removed the DataFeed class.

--- Source/Millistream.Streaming/ConnnectionStatusChangedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Millistream.Streaming
-{
-    /// <summary>
-    /// Represents the method that will handle an event which provides connection status data.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">An object that contains the connection status data.</param>
-    public delegate void ConnnectionStatusChangedEventHandler(object sender, ConnectionStatusChangedEventArgs e);
-}
