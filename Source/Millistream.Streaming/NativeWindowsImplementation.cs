0ff3fbf0f98b9997b1b375e67ecf3426085ce2fa
mgnsm <mgns_m@yahoo.com>
2021-01-28 22:34:15 +0100

    Replaced the string marshalling with a pointer to the ASCII bytes.

--- Source/Millistream.Streaming/NativeWindowsImplementation.cs
+++ Source/Millistream.Streaming/NativeWindowsImplementation.cs
@@ -65,7 +65,7 @@ namespace Millistream.Streaming
             public static extern void mdf_message_destroy(IntPtr message);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
-            public static extern int mdf_message_add_list(IntPtr message, uint tag, string value);
+            public static extern int mdf_message_add_list(IntPtr message, uint tag, IntPtr value);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_add_numeric(IntPtr message, uint tag, IntPtr value);
@@ -83,13 +83,13 @@ namespace Millistream.Streaming
             public static extern int mdf_message_add_string2(IntPtr message, uint tag, IntPtr value, int len);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
-            public static extern int mdf_message_add_date(IntPtr message, uint tag, string value);
+            public static extern int mdf_message_add_date(IntPtr message, uint tag, IntPtr value);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_add_date2(IntPtr message, uint tag, int year, int mon, int day);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
-            public static extern int mdf_message_add_time(IntPtr message, uint tag, string value);
+            public static extern int mdf_message_add_time(IntPtr message, uint tag, IntPtr value);
 
             [DllImport(DllName, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
             public static extern int mdf_message_add_time2(IntPtr message, uint tag, int hour, int min, int sec, int msec);
@@ -134,14 +134,14 @@ namespace Millistream.Streaming
         public int mdf_get_property(IntPtr handle, MDF_OPTION option, ref ulong value) => NativeWindowsMethods.mdf_get_property(handle, option, ref value);
         public int mdf_get_property(IntPtr handle, MDF_OPTION option, ref long value) => NativeWindowsMethods.mdf_get_property(handle, option, ref value);
         public int mdf_message_add(IntPtr message, ulong instrument_reference, int message_reference) => NativeWindowsMethods.mdf_message_add(message, instrument_reference, message_reference);
-        public int mdf_message_add_date(IntPtr message, uint tag, string value) => NativeWindowsMethods.mdf_message_add_date(message, tag, value);
+        public int mdf_message_add_date(IntPtr message, uint tag, IntPtr value) => NativeWindowsMethods.mdf_message_add_date(message, tag, value);
         public int mdf_message_add_date2(IntPtr message, uint tag, int year, int mon, int day) => NativeWindowsMethods.mdf_message_add_date2(message, tag, year, mon, day);
         public int mdf_message_add_int(IntPtr message, uint tag, long value, int decimals) => NativeWindowsMethods.mdf_message_add_int(message, tag, value, decimals);
-        public int mdf_message_add_list(IntPtr message, uint tag, string value) => NativeWindowsMethods.mdf_message_add_list(message, tag, value);
+        public int mdf_message_add_list(IntPtr message, uint tag, IntPtr value) => NativeWindowsMethods.mdf_message_add_list(message, tag, value);
         public int mdf_message_add_numeric(IntPtr message, uint tag, IntPtr value) => NativeWindowsMethods.mdf_message_add_numeric(message, tag, value);
         public int mdf_message_add_string(IntPtr message, uint tag, IntPtr value) => NativeWindowsMethods.mdf_message_add_string(message, tag, value);
         public int mdf_message_add_string2(IntPtr message, uint tag, IntPtr value, int len) => NativeWindowsMethods.mdf_message_add_string2(message, tag, value, len);
-        public int mdf_message_add_time(IntPtr message, uint tag, string value) => NativeWindowsMethods.mdf_message_add_time(message, tag, value);
+        public int mdf_message_add_time(IntPtr message, uint tag, IntPtr value) => NativeWindowsMethods.mdf_message_add_time(message, tag, value);
         public int mdf_message_add_time2(IntPtr message, uint tag, int hour, int min, int sec, int msec) => NativeWindowsMethods.mdf_message_add_time2(message, tag, hour, min, sec, msec);
         public int mdf_message_add_time3(IntPtr message, uint tag, int hour, int min, int sec, int nsec) => NativeWindowsMethods.mdf_message_add_time3(message, tag, hour, min, sec, nsec);
         public int mdf_message_add_uint(IntPtr message, uint tag, ulong value, int decimals) => NativeWindowsMethods.mdf_message_add_uint(message, tag, value, decimals);
